59d2d363375f94c3122c71a3df4eb35b5e8a58e4
seahahn <seah.ahn.nt@gmail.com>
2024-02-18 21:24:59 +0100

    🔧 prettier config created

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+const config = {
+  endOfLine: "lf",
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: false,
+  bracketSameLine: true,
+};
+
+export default config;
