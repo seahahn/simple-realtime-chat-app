417a4b420b49bf39ff459e6d4ca0fb7c7a4df7d3
seahahn <seah.ahn.nt@gmail.com>
2024-02-19 16:15:57 +0100

    🛂 auth related functions created

---
+++ app/lib/auth.server.ts
@@ -0,0 +1,127 @@
+import bcrypt from "bcryptjs";
+import {db} from "./db.server";
+import {createCookieSessionStorage, redirect} from "@remix-run/node";
+import {SESSION_SECRET, USER_SESSION_KEY} from "~/constants/envs";
+
+interface SignInForm {
+  email: string;
+  password: string;
+}
+
+interface SignUpForm extends SignInForm {
+  nickname: string;
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "Sailrs_session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [SESSION_SECRET],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, remember: boolean, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set(USER_SESSION_KEY, userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session, {
+        // if remember is true, the session will persist for 7 days
+        // if remember is false, the session will persist for the browser session
+        maxAge: remember ? 60 * 60 * 24 * 7 : undefined,
+      }),
+    },
+  });
+}
+export async function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get("Cookie"));
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get(USER_SESSION_KEY);
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect(`/login?${searchParams}`);
+  }
+  return userId;
+}
+
+export async function getUserId(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get(USER_SESSION_KEY);
+  if (!userId || typeof userId !== "string") return null;
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "string") return null;
+
+  try {
+    const user = await db.user.findUnique({
+      where: {id: userId},
+      select: {id: true, email: true, nickname: true},
+    });
+    return user;
+  } catch {
+    throw signOut(request);
+  }
+}
+
+/**
+ * Check if the user with the given email exists in the database
+ * @param email - email of the user to check
+ * @returns true if user exists, false otherwise
+ */
+export async function checkUserEmail(email: string) {
+  const userExists = await db.user.findUnique({
+    where: {email},
+  });
+
+  return userExists ? true : false;
+}
+
+export async function checkUserNickname(nickname: string) {
+  const userExists = await db.user.findUnique({
+    where: {nickname},
+  });
+
+  return userExists ? true : false;
+}
+
+export async function signUp({email, password, nickname}: SignUpForm) {
+  const passwordHash = await bcrypt.hash(password, 10);
+  const user = await db.user.create({
+    data: {email, password: passwordHash, nickname},
+  });
+  return {id: user.id, email};
+}
+
+export async function signIn({email, password}: SignInForm) {
+  const user = await db.user.findUnique({
+    where: {email},
+  });
+  if (!user) return null;
+
+  const isCorrectPassword = await bcrypt.compare(password, user.password);
+  if (!isCorrectPassword) return null;
+
+  return {id: user.id, email: user.email, nickname: user.nickname};
+}
+
+export async function signOut(request: Request) {
+  const session = await getUserSession(request);
+  return redirect("/", {
+    headers: {
+      "Set-Cookie": await storage.destroySession(session),
+    },
+  });
+}
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {json} from "@remix-run/node";
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function validateUrl(url: string) {
+  const urls = ["/"];
+  if (urls.includes(url)) {
+    return url;
+  }
+  return "/";
+}
+export const badRequest = <T>(data: T) => json(data, {status: 400});
