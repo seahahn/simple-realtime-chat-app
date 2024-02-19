7e769418ca5a29f42b9f4a4d1f9f48f76932287a
seahahn <seah.ahn.nt@gmail.com>
2024-02-19 11:17:55 +0100

    🌱 texts constants file created

---
+++ app/constants/texts.ts
@@ -0,0 +1,23 @@
+export default {
+  APP_NAME: "App for Sailrs",
+  APP_DESCRIPTION: "Welcome to Gyeongho's App",
+
+  SIGN_IN: "Sign In",
+  SIGN_UP: "Sign Up",
+  SIGN_OUT: "Sign Out",
+  RESET_PASSWORD: "Reset Password",
+  USER_PROFILE: "User Profile",
+
+  SAVE_CHANGES: "Save Changes",
+  DELETE_USER: "Delete User",
+
+  PASSWORD_GUIDE:
+    "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.",
+
+  SIGN_UP_SUCCESS_TITLE: "Account Created Successfully",
+  SIGN_UP_SUCCESS_DESCRIPTION: "Your account has been successfully created!",
+
+  RESET_PASSWORD_SUCCESS_TITLE: "Reset Password Email Sent",
+  RESET_PASSWORD_SUCCESS_DESCRIPTION:
+    "An email with instructions to reset your password has been sent to your email address.",
+};
