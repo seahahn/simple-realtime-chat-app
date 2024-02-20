25c36ec0e3e34699d0d566e6017b8ec9c7f395b1
seahahn <seah.ahn.nt@gmail.com>
2024-02-20 02:28:30 +0100

    🗃️ prisma client setting reverted

--- app/lib/db.server.ts
+++ app/lib/db.server.ts
@@ -11,13 +11,7 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+  db = new PrismaClient();
 } else {
   if (!global.__db) {
     global.__db = new PrismaClient();
